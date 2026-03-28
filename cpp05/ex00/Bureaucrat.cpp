3418ede3890b0580778b5b7deb5f214cc8d15792
mrouissy <rouissy179@gmail.com>
2026-03-28 14:19:09 +0100

    adding class bureaucrat

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -6,7 +6,32 @@
 /*   By: mrouissy <mrouissy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/28 13:38:38 by mrouissy          #+#    #+#             */
-/*   Updated: 2026/03/28 13:38:39 by mrouissy         ###   ########.fr       */
+/*   Updated: 2026/03/28 14:17:11 by mrouissy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+
+Bureaucrat::Bureaucrat(std::string name, int grade)
+{
+    
+}
+
+Bureaucrat::Bureaucrat()
+{    
+}
+Bureaucrat::~Bureaucrat()
+{
+    
+}
+
+
+std::string Bureaucrat::getname() const
+{
+    return this->name;
+}
+int Bureaucrat::get_grade() const
+{
+    return this->grade;
+}
--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -6,7 +6,26 @@
 /*   By: mrouissy <mrouissy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/28 13:38:44 by mrouissy          #+#    #+#             */
-/*   Updated: 2026/03/28 13:38:45 by mrouissy         ###   ########.fr       */
+/*   Updated: 2026/03/28 14:15:29 by mrouissy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT
+#define BUREAUCRAT
+
+#include <iostream>
+#include <string>
+
+class Bureaucrat
+{
+    private:
+        std::string name;
+        int grade;
+    public:
+        Bureaucrat();
+        Bureaucrat(std::string name, int grade);
+        ~Bureaucrat();
+        std::string getname() const;
+        int get_grade() const;
+};
+#endif
