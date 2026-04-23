c30cad88b15baa7abb97048d3f6c605928805ff9
mrouissy <rouissy179@gmail.com>
2026-04-23 14:40:17 +0100

    .

--- cpp06/ex00/ScalarConverte.cpp
+++ cpp06/ex00/ScalarConverte.cpp
@@ -6,7 +6,7 @@
 /*   By: mrouissy <mrouissy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 13:33:27 by mrouissy          #+#    #+#             */
-/*   Updated: 2026/04/21 13:14:13 by mrouissy         ###   ########.fr       */
+/*   Updated: 2026/04/23 14:39:54 by mrouissy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,68 +35,100 @@ ScalarConverte::~ScalarConverte()
 }
 
 
-void print_data(char c, int i, float f, double d)
-{
-    if(c == -1 )
-    {
-        std::cout << "unvalid data." << std::endl;
-        return ;
-    }
-    std::cout << "char : "   << c << std::endl;
-    std::cout << "int : "    << i << std::endl;
-    std::cout << "float : "  << f << std::endl;
-    std::cout << "double : " << d << std::endl;
-}
-int check_type(std::string data)
-{
-    if(data.length() == 1 && !isdigit(0))
-        return T_CHAR;
-    else if(data.find('.'))
-    {
-        if(data[data.size() - 1] == 'f')
-            return T_FLOAT;
-        else
-            return T_DOUBLE;
-    }
-    return T_INT;
-} 
-int char_convert(std::string data)
+// void print_data(char c, int i, float f, double d)
+// {
+//     if(c == -1 )
+//     {
+//         std::cout << "unvalid data." << std::endl;
+//         return ;
+//     }
+//     std::cout << "char : "   << c << std::endl;
+//     std::cout << "int : "    << i << std::endl;
+//     std::cout << "float : "  << f << std::endl;
+//     std::cout << "double : " << d << std::endl;
+// }
+// int check_type(std::string data)
+// {
+//     if(data.length() == 1 && !isdigit(0))
+//         return T_CHAR;
+//     else if(data.find('.'))
+//     {
+//         if(data[data.size() - 1] == 'f')
+//             return T_FLOAT;
+//         else
+//             return T_DOUBLE;
+//     }
+//     return T_INT;
+// } 
+
+void char_convert(std::string data)
 {
     const char *tmp = data.c_str();
-    if(tmp[0] <33 || tmp[0] > 126)
+    if(tmp[0] < 33)
     {
         std::cout << "Non-printable characters." << std::endl;
-        return -1;
+        return;
     }
-    return static_cast<char>(tmp[0]);
+    std::cout << "char : " << static_cast<char>(tmp[0])<< std::endl;;
 }
 
-void ScalarConverte::convert(std::string data)
+bool check_imposible(std::string data)
 {
-    char c; int i;
     if(data.empty())
     {
         std::cout << "string is empty." << std::endl;
+        return true;
+    }
+    else if(data[0]<0 || data[0] > 127 || data[0] == '\0')
+    {
+        std::cout << "imposible" << std::endl;
+        return true;
+    }
+    return false;
+}
+void int_convert(std::string data)
+{
+    char *rest;
+    int to_cast = strtod(data.c_str(), &rest);
+
+    if(rest && rest != "f")
+    {
+        std::cout << "can't  convert. " << std::endl;
+        return ;
+    }
+    std::cout << "int :" << static_cast<int>(to_cast) << std::endl;
+}
+void float_convert(std::string data)
+{
+    char *rest;
+    int to_cast = strtod(data.c_str(), &rest);
+    if(data.find('.'))
+    {
+        if(rest != "f")
+        {
+            std::cout << "imposible" << std::endl; 
+            return;
+        }
+        std::cout << "float: " << static_cast<float>(to_cast) << "f" << std::endl; 
+    }
+    else if(!rest)
+    {
+        std::cout << "float: " << static_cast<float>(to_cast) << ".f" << std::endl; 
         return;
     }
-    switch (check_type(data))
+    else if(rest == "nanf" || "nan")
     {
-        case T_CHAR:
-            c =char_convert(data);
-            break;
-        case T_INT:
-            /* code */
-            break;
-        case T_FLOAT:
-            /* code */
-            break;
-        case T_DOUBLE:
-            /* code */
-            break;
-        default:
-            break;
+        std::cout << "float: nanf" << std::endl; 
     }
+    else
+        std::cout << "imposible " << std::endl;
+}
+void ScalarConverte::convert(std::string data)
+{
+   
+    if(check_imposible(data))
+        return ;
+    char_convert(data);
     
-    print_data(c,c,c,c);
 }
         
---
+++ cpp06/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    char *rest;
+
+    cout << "data ===>" << static_cast<float>(12.6f) << "\nrest ===> " << rest << endl; 
+
+    return 0;
+}
